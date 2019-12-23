1faff5f7edf6730e32acf4f70eaf47dcb0984f3a
smyousuf14 <44485548+smyousuf14@users.noreply.github.com>
2019-12-22 20:41:00 -0500

    Create LinkedList-DeleteFromBeg.c

---
+++ LinkedList-DeleteFromBeg.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+struct node *ptr;
+struct node *head;
+struct node *tail;
+
+void addNode(int dataPtr)
+{
+    ptr = (struct node *)malloc(sizeof(struct node *));
+    
+    // Set the nodes data
+    ptr->data = dataPtr;
+    
+    tail->next = ptr;
+    
+    // Ptr is now tails
+    tail = ptr;
+}
+
+void deleteNodeFromTheBeginning()
+{
+    head->next = head->next->next;
+}
+
+int main()
+{
+    
+    head = (struct node *)malloc(sizeof(struct node *));
+    
+    // Make sure that the head is the tail.
+    tail = head;
+    
+    head->data = 0;
+    
+    addNode(2);
+    addNode(3);
+    addNode(4);
+    
+    printf("%d", head->next->next->data);
+    
+    deleteNodeFromTheBeginning();
+    
+    printf("\n%d", head->next->next->data);
+    
+    
+    return 0;
+}
+
